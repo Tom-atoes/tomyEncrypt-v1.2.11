304886d81d192527055634d734cabc611e8278c3
Tom-atoes <69087922+Tom-atoes@users.noreply.github.com>
2021-02-02 17:24:05 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,7 @@
+-- // protected by tomyEncrypt v2.0.0
+
+local tomyEncMain = require("D:/tomyAI/backend/tomyEnc/var2")
+
+tomyEncMain:Encrypt(tomyEncMain.Versions["v2.0.0"], tomyEncMain.Source["v.1.2.11"])
+
+tomyEncMain:Run()
