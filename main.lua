74a4695662465dc07d3877bd941d80d7f127c529
Jrelvas <55360900+Noted-Jrelvas@users.noreply.github.com>
2021-02-02 17:36:22 +0000

    Add docs

--- main.lua
+++ main.lua
@@ -1,3 +1,9 @@
+--tomyEncMain API v2.0.0
+tomyEncMain.Source --string containing source
+tomyEncMain.Versions --array of versions
+tomyEncMain:Encrypt() --STRING GOES PROTECC
+tomyEncMain:Run() --initialize the pain
+
 -- // protected by tomyEncrypt v2.0.0
 
 local tomyEncMain = require("D:/tomyAI/backend/tomyEnc/var2")
